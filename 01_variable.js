b835b28f02500446fe6d0d3df39bb31270a4221b
Aparmit <aparmitsrivastav01@gmail.com>
2026-06-01 12:48:46 +0530

    feat: add javascript variables basics

---
+++ 01_variable.js
@@ -0,0 +1,23 @@
+const accountId = 144553 // const immutable
+let accountEmail = "xyz@gmail.com" // variable 2 tareke let or var 
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+{
+    // scope he ye
+}
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+
+
+// accountId = 2 // immutable
+accountEmail = "change@gmail.com"
+accountPassword = "$$$$$"
+accountCity = "lucknow"
+// console.log(accountEmail);
+console.table([accountEmail,accountId,accountPassword,accountCity]) //array pass hogi para me 
