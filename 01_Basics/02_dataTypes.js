2a63e55b2f7ef61e9f29f62d2b54512ff418799a
Aparmit <aparmitsrivastav01@gmail.com>
2026-06-03 12:01:45 +0530

    Conversionoperation

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(4+5)  we are using nodejs , not browser 
+
+console.log(3+3); 
+// code readability maintain
+console.log("APPY")
+
+let name = "appy"
+let age = 18
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string '' ""
+// boolean => true false
+// null => standalone value / ye khali h
+// undefined => define nahi kiya gaya
+// symbol - jab  react use krenge tb use ayega ye || unique 
+
+// object
+
+console.log(typeof 5);
+console.log(typeof null);
+console.log(typeof undefined); //important for interview
---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = undefined
+// console.log(typeof score)
+
+let valuInNumber = Number(score)
+// console.log(typeof valuInNumber);
+// console.log(valuInNumber);
+//NAN = NOT AN NUMBER
+// null == 0
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(isLoggedIn)
+console.log(boolenIsLoggedIn)
+
+// 1 => true; 0 => false 
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33 
+let StringNumber = String(someNumber)
+console.log(typeof StringNumber);
