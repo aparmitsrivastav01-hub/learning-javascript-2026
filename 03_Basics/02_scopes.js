de94d096b79068683251cb31e177b9f00a199f84
Aparmit <aparmitsrivastav01@gmail.com>
2026-06-11 01:11:50 +0530

    end day 6

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,20 @@
+a = 300
+if (true) {
+  let a = 10;
+  const b = 20;
+  console.log(`Inner, ${a}`)
+}
+
+//block scope andar 
+//gloabl scope bahar
+// Avoid var
+
+
+{
+  //scope hua ye
+}
+
+
+console.log(a)
+// console.log(c)
+// console  k andar scope alag or local par node se scope alag 
