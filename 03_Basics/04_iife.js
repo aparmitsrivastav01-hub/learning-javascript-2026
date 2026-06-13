ff932255fb72850726633c3e19e8f23f607fbf40
Aparmit <aparmitsrivastav01@gmail.com>
2026-06-13 14:13:25 +0530

    day 9

--- 03_Basics/04_iife.js
+++ 03_Basics/04_iife.js
@@ -1,9 +1,13 @@
 // Immediately Invoked Function Express 
 
 (function chai(){
+    // named iifee
     console.log('DB Connected');
-})()
+})();
 
-
+( (name)=>{
+    //unnamed iife
+    console.log(`DB CONNECTED TWO ${name}`)
+}) ('aparmit')
 
 // (defination)(execution)
