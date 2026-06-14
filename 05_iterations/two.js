a7e70f0d368b4745d7284ea9cdeecd4aa3d64964
Aparmit <aparmitsrivastav01@gmail.com>
2026-06-14 18:08:33 +0530

    day 10

---
+++ 05_iterations/two.js
@@ -0,0 +1,29 @@
+// let index = 0
+
+// while (index <=10){
+//     console.log(`Value of Index IS ::: ${index}`)
+//     index += 2
+// }
+
+let BigArr = ['flash','superman','gandphadman']
+let arr = 0
+while(arr<BigArr.length){
+    console.log(BigArr[arr])
+    arr += 1
+}
+
+
+let score = 1;
+
+
+do {
+    console.log(`Score is :: ${score}`)
+    score++
+} while (score<=10);
+let score1 = 10;
+
+
+do {
+    console.log(`Score is :: ${score}`)
+    score++
+} while (score<=10);
