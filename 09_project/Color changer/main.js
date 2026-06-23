6162dda2e4642bba3d42e7f09a8a7b9d732557f4
Aparmit <aparmitsrivastav01@gmail.com>
2026-06-23 15:09:28 +0530

    project 4

---
+++ 09_project/Color changer/main.js
@@ -0,0 +1,31 @@
+//generate a random color 
+
+const randomcolor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for (let i = 0; i<6 ; i++ ){
+        color += hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+};
+
+let intervalId
+const startChanginColor = function(){
+    if(!intervalId){
+        intervalId = setInterval(changeBgColor,1000);
+    }
+
+    function changeBgColor(){
+        document.body.style.backgroundColor = randomcolor();
+    } 
+    
+};
+const stopChanginColor = function(){
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+
+
+document.querySelector("#start").addEventListener("click",startChanginColor)
+document.querySelector("#stop").addEventListener("click",stopChanginColor)
